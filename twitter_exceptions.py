f8f7eb66f00841bb0de570df6faaf8d05731e739
Ferdinand E. Silva <ferdie_v143@yahoo.com>
2012-07-08 22:03:33 +0800

    add script info

--- twitter_exceptions.py
+++ twitter_exceptions.py
@@ -1,3 +1,11 @@
+"""
+*************************************************************************************
+* @author: Ferdinand E. Silva
+* @email: ferdinandsilva@ferdinandsilva.com
+* @title: Rhythmbox To Twitter
+* @description: Post the title of currently playing song of Rhythm Box to Twitter
+*************************************************************************************
+"""
 class InvalidTwitterAccountException(Exception):
 	pass
 	
